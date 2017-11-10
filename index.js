996ca83cac58774e2e5f9e796a4a715c722a760d
Bushra Taimoor <bushra376@hotmail.com>
2017-11-10 21:52:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function happyHolidays(){
   return "Happy holidays!"
 }
  function happyHolidaysTo(name){
-   return `Happy holidays ${name}!`
+  return 'Happy holidays ${name}!'
+  
  }
  function happyHolidaysTo(holiday, name){
    return `Happy ${holiday}, ${name}!`
